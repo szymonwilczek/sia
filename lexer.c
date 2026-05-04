4f541538fbf8aa2f26cbabd82351991066519182
Szymon Wilczek <swilczek.lx@gmail.com>
2026-05-04 23:36:23 +0200

    feat(lexer): add tokenizer for mathematical expressions
    Sia needs to convert raw input strings into a token stream before parsing.
    
    Implement Lexer supporting numbers (int/float), identifiers (variables,
    function names), arithmetic operators (+, -, *, /, ^), parentheses,
    commas, and EOF/error tokens. Uses strtod for robust number parsing.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

---
+++ lexer.c
@@ -0,0 +1,105 @@
+#include "lexer.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+void lexer_init(Lexer *lex, const char *source) {
+  lex->source = source;
+  lex->pos = 0;
+}
+
+static Token make_token(TokenType type, const char *start, size_t length) {
+  Token t = {.type = type, .start = start, .length = length, .numval = 0.0};
+  return t;
+}
+
+static Token make_number(const char *start, size_t length, double val) {
+  Token t = {
+      .type = TOK_NUMBER, .start = start, .length = length, .numval = val};
+  return t;
+}
+
+static Token make_error(const char *start) {
+  Token t = {.type = TOK_ERROR, .start = start, .length = 1, .numval = 0.0};
+  return t;
+}
+
+Token lexer_next(Lexer *lex) {
+  const char *s = lex->source;
+
+  while (s[lex->pos] && isspace((unsigned char)s[lex->pos]))
+    lex->pos++;
+
+  if (!s[lex->pos])
+    return make_token(TOK_EOF, s + lex->pos, 0);
+
+  const char *start = s + lex->pos;
+  char c = s[lex->pos];
+
+  if (isdigit((unsigned char)c) || c == '.') {
+    char *end;
+    double val = strtod(start, &end);
+    size_t len = (size_t)(end - start);
+    lex->pos += len;
+    return make_number(start, len, val);
+  }
+
+  if (isalpha((unsigned char)c) || c == '_') {
+    size_t begin = lex->pos;
+    while (isalnum((unsigned char)s[lex->pos]) || s[lex->pos] == '_')
+      lex->pos++;
+    return make_token(TOK_IDENT, start, lex->pos - begin);
+  }
+
+  lex->pos++;
+  switch (c) {
+  case '+':
+    return make_token(TOK_PLUS, start, 1);
+  case '-':
+    return make_token(TOK_MINUS, start, 1);
+  case '*':
+    return make_token(TOK_STAR, start, 1);
+  case '/':
+    return make_token(TOK_SLASH, start, 1);
+  case '^':
+    return make_token(TOK_CARET, start, 1);
+  case '(':
+    return make_token(TOK_LPAREN, start, 1);
+  case ')':
+    return make_token(TOK_RPAREN, start, 1);
+  case ',':
+    return make_token(TOK_COMMA, start, 1);
+  default:
+    return make_error(start);
+  }
+}
+
+const char *token_type_name(TokenType type) {
+  switch (type) {
+  case TOK_NUMBER:
+    return "NUMBER";
+  case TOK_IDENT:
+    return "IDENT";
+  case TOK_PLUS:
+    return "PLUS";
+  case TOK_MINUS:
+    return "MINUS";
+  case TOK_STAR:
+    return "STAR";
+  case TOK_SLASH:
+    return "SLASH";
+  case TOK_CARET:
+    return "CARET";
+  case TOK_LPAREN:
+    return "LPAREN";
+  case TOK_RPAREN:
+    return "RPAREN";
+  case TOK_COMMA:
+    return "COMMA";
+  case TOK_EOF:
+    return "EOF";
+  case TOK_ERROR:
+    return "ERROR";
+  }
+  return "UNKNOWN";
+}
---
+++ lexer.h
@@ -0,0 +1,37 @@
+#ifndef SIA_LEXER_H
+#define SIA_LEXER_H
+
+#include <stddef.h>
+
+typedef enum {
+  TOK_NUMBER,
+  TOK_IDENT,
+  TOK_PLUS,
+  TOK_MINUS,
+  TOK_STAR,
+  TOK_SLASH,
+  TOK_CARET,
+  TOK_LPAREN,
+  TOK_RPAREN,
+  TOK_COMMA,
+  TOK_EOF,
+  TOK_ERROR
+} TokenType;
+
+typedef struct {
+  TokenType type;
+  const char *start;
+  size_t length;
+  double numval;
+} Token;
+
+typedef struct {
+  const char *source;
+  size_t pos;
+} Lexer;
+
+void lexer_init(Lexer *lex, const char *source);
+Token lexer_next(Lexer *lex);
+const char *token_type_name(TokenType type);
+
+#endif
