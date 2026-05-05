2c9534079cda38ae01ad718ce3327143b1db5eb0
Szymon Wilczek <swilczek.lx@gmail.com>
2026-05-05 23:40:03 +0200

    chore: bump version to 0.6.6
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- main.c
+++ main.c
@@ -883,7 +883,7 @@ static int process_input(const char *input, int batch_mode) {
 
 static void repl(void) {
   char line[4096];
-  fprintf(stderr, "sia v0.6.0 - type an expression, or 'quit' to exit\n");
+  fprintf(stderr, "sia v0.6.6 - type an expression, or 'quit' to exit\n");
 
   for (;;) {
     fprintf(stdout, "sia> ");
